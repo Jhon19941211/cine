dd4b12608892729ce6e6f2d070008b7ba205c184
Juan Esteban Narvaez Maya <juanenarvaez@umariana.edu.co>
2019-03-31 22:41:16 -0500

    Cambio de interfaz - Cards de cartelera

--- public/js/logica_cartelera.js
+++ public/js/logica_cartelera.js
@@ -1,11 +1,41 @@
 
 $.ajax({
-	url: "http://ocioavila.com/api/v1/movies/theaters?access_token=0MOYE2KGIULJS0OOOM8V8GU7HTOYCHQ5K2QILBQA64U7R6SGZ00SD2G2H51N1RMDYL84WKK01UL3G958Z2RQKT84PPC26IRUZEQQF1CRNCS17EVHOVVBWHGIG90XBEDIUVXIFR4O0ETNZALD21NEYDTRSLZH18CPN2RZXYGQUYXMCH3V7768JAW8FWEUQ403Y9OM7ILJ769BVEF0GT",
+	url: "https://api.themoviedb.org/3/discover/movie?api_key=c1dece2ed8e7859a019d7ccb2d343be6&language=es-CO&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2018",
 	type: "GET",
 	success: function(data){
 		console.log(data);
+
+		var active = "active";
+		$.each(data.results, function(i, item) {
+		    console.log(item);
+
+			$('#accion1').append(
+				'<div class="carousel-item '+active+'">'+
+					'<div class="container">'+
+						'<div class="row justify-content-md-center">'	+								
+							'<div class="card" style="width: 50%;">'+
+								'<img src="https://image.tmdb.org/t/p/w500/'+item.backdrop_path+'" class="d-block w-100" alt="...">'+
+								'<div class="card-body">'+
+									'<h5 class="card-title">'+item.original_title+'</h5>'+
+									'<p class="card-text">'+item.overview+'</p>'+
+									'<a href="#" class="btn btn-primary">Reservar</a>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
+				'</div>'
+			);
+			active="no";
+		});
+		active="active";
 	},
 	error: function(data){
 
 	}
 })	
+
+// imagenes
+// https://image.tmdb.org/t/p/w500/
+
+//generos
+// https://api.themoviedb.org/3/genre/movie/list?api_key=c1dece2ed8e7859a019d7ccb2d343be6&language=es-CO
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,201 +4,119 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Cine</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-   folder instead of downloading all of them to reduce the load. -->
-   <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-   <!-- Morris chart -->
-   <link rel="stylesheet" href="bower_components/morris.js/morris.css">
-   <!-- jvectormap -->
-   <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
-   <!-- Date Picker -->
-   <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-   <!-- Daterange picker -->
-   <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
-   <!-- bootstrap wysihtml5 - text editor -->
-   <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">  
+  {{-- <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">   --}}
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">  
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">  
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">  
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
-<!-- Google Font -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">   
+  <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">   
+  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">   
+  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition sidebar-mini">
   <div class="wrapper">
 
-    <header class="main-header">
-      <!-- Logo -->
-      <a href="index2.html" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>C</b>INE</span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>CINE</b>NARV</span>
-      </a>
-      <!-- Header Navbar: style can be found in header.less -->
-      <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+    <header class="main-header">      
+      <a class="logo bg-light">        
+        <span class="logo-mini text-dark"><b>C</b>INE</span>        
+        <span class="logo-lg text-dark" ><b>CINE</b>NARV</span>
+      </a>      
+      
+      <nav class="navbar navbar-static-top bg-primary">        
+        {{-- <a class="sidebar-toggle img-circle bg-primary" data-toggle="push-menu" role="button">
           <span class="sr-only">Toggle navigation</span>
-        </a>
-
-        <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
-            <!-- Messages: style can be found in dropdown.less-->
-
-            <!-- User Account: style can be found in dropdown.less -->
-            <li class="dropdown user user-menu">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="img/user.png" class="user-image" alt="User Image">
-                <span class="hidden-xs">{{ Auth::user()->name }}</span>
-              </a>
-              <ul class="dropdown-menu">
-                <!-- User image -->
-                <li class="user-header">
-                  <img src="img/user.png" class="img-circle" alt="User Image">
-
-                  <p>
-                    {{ Auth::user()->name }}
-
-                  </p>
-                </li>
-                <!-- Menu Body -->
-                <!-- Menu Footer-->
-                <li class="user-footer">
-                  
-                  <div class="pull-right">
-                    <!-- <a href="#" class="btn btn-default btn-flat">Sign out</a> -->
-                    <a class="btn btn-default btn-flat" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                    {{ __('Cerrar sesión') }}
-                  </a>
-
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                  </form>
-                </div>
-              </li>
-            </ul>
-          </li>
-          
-        </ul>
+        </a> --}}
+        <a href=""></a>
+        
+        <div class="btn-group pull-right">
+          <div class="btn-group dropleft" role="group">
+            <button type="button" class="btn btn-light  dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">                
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+            Cerrar sesión</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+            <a class="dropdown-item" href="#">Configuración</a>
+          </div>
+        </div>
+        <button type="button" class="btn btn-light ">
+          <strong>{{ Auth::user()->name }}</strong>
+        </button>
       </div>
     </nav>
-  </header>
-  <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-      <!-- Sidebar user panel -->
+  </header>  
+
+  <aside class="main-sidebar bg-light">    
+    <section class="sidebar">      
       <div class="user-panel">
         <div class="pull-left image">
           <img src="img/user.png" class="img-circle" alt="User Image">
         </div>
-        <div class="pull-left info">
+        <div class="pull-left info text-dark">
           <p>{{ Auth::user()->name }}</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
-      <!-- search form -->
+          <a href="#" class="text-dark"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>            
+      </div>            
 
-      <!-- /.search form -->
-      <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header" style="color: white">MENÚ CINE</li>
-   
-         <li class="active"><a href="{{ route('cartelera.index') }}"><i class="fas fa-video"></i>   Cartelera</a></li>
-         <li class="active"><a href="index.html"><i class="fas fa-film"></i> Tus reservas</a></li>
-         <li class="active"><a href="index.html"><i class="fas fa-users-cog"></i> Perfil</a></li>
-         <!-- <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li> -->
+      <ul class="sidebar-menu bg-light">
+        <strong><li class="header bg-light text-dark">MENÚ CINE</li></strong>
 
-
+        <li><a style="color: black; text-decoration: none;" href="{{ route('cartelera.index') }}"><i class="fas fa-video">&nbsp;&nbsp;</i>Cartelera</a></li>
+        <li><a style="color: black; text-decoration: none;" href="#"><i class="fas fa-film">&nbsp;&nbsp;</i> Tus reservas</a></li>
+        <li><a style="color: black; text-decoration: none;" href="#"><i class="fas fa-users-cog">&nbsp;&nbsp;</i> Perfil</a></li>         
       </ul>
-    </section>
-    <!-- /.sidebar -->
+    </section>    
   </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-   
-
-    <!-- Main content -->
+  
+  <div class="content-wrapper">    
     <section class="content">
       @yield('content')
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    </section>    
+  </div>  
   <footer class="main-footer">
-   
-    <strong>Copyright &copy; 2019 <a href="https://adminlte.io">CALVO STUDIOS</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; 2019 <strong>CALVO STUDIOS.</strong></strong>
   </footer>
+  <div class="control-sidebar-bg"></div>
+</div>
 
-  <!-- Control Sidebar -->
-  
-  <!-- /.control-sidebar -->
-  <!-- Add the sidebar's background. This div must be placed
-   immediately after the control sidebar -->
-   <div class="control-sidebar-bg"></div>
- </div>
- <!-- ./wrapper -->
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
- <!-- jQuery 3 -->
- <script src="bower_components/jquery/dist/jquery.min.js"></script>
- <!-- jQuery UI 1.11.4 -->
- <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
- <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
- <script>
+<script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="bower_components/raphael/raphael.min.js"></script>
-<script src="bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
 <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
 <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
 <script src="bower_components/moment/min/moment.min.js"></script>
 <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
 
 <!-- SCRIPT CARTELERA -->
 <script src="js/logica_cartelera.js"></script>
+
 </body>
 </html>
